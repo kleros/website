3e5b073c1c9dc0a9976bc675161e483a20e161ca
Nicolas Wagner <contact@wagner-nicolas.com>
2019-05-02 14:11:01 +0300

    fix: link media > download book

--- src/pages/media.js
+++ src/pages/media.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import get from 'lodash/get'
 import Helmet from 'react-helmet'
+import Modal from 'react-responsive-modal'
 import theMeta from '../js/helpers.js'
 
 class RootIndex extends React.Component {
@@ -12,7 +13,8 @@ class RootIndex extends React.Component {
 			mediaFilter: {
 				value: '',
 				key: Math.random()
-			}
+			},
+			open: false
 		}
 		//this.someName = this.someName.bind(this);
 	}
@@ -25,6 +27,15 @@ class RootIndex extends React.Component {
 			}
 		});
 	}
+
+	onCloseModal = () => {
+    this.setState({ open: false });
+	};
+	
+	onOpenModal = () => {
+    this.setState({ open: true });
+	};
+
 	render() {
 		var siteTitle = "Kleros";
 		var media = get(this, 'props.data.allContentfulMediaEntry.edges');
@@ -35,63 +46,74 @@ class RootIndex extends React.Component {
 		return (
 			<div>
 				<Helmet><html lang="en" /><title>{siteTitle}</title></Helmet>
-					<section className="ca_juror_top ca_media_top">
-						<div className="container">
-							<div className="row">
-								<div className="col-12 col-md-7">
-									<h1>{theMeta(data.topBlock, 'Media > Top > Title').data.data}</h1>
-									<h6>{theMeta(data.topBlock, 'Media > Top > Sub Title').data.data}</h6>
-								</div>
-								<div className="col-12 col-md-5"></div>
+				<Modal open={this.state.open} onClose={this.onCloseModal} center classNames={{
+						modal: 'customModal'
+					}}
+				>
+					<h2>Download Book</h2>
+					<div className="ca_book">
+						<a href="/book/Dispute-Resolution-Kleros.epub" target="_blank">EPUP FORMAT</a>
+						<a href="/book/Dispute-Resolution-Kleros.pdf" target="_blank">PDF FORMAT</a>
+						<a href="/book/Dispute-Resolution-Kleros.mobi" target="_blank">MOBI FORMAT</a>
+					</div>
+				</Modal>
+				<section className="ca_juror_top ca_media_top">
+					<div className="container">
+						<div className="row">
+							<div className="col-12 col-md-7">
+								<h1>{theMeta(data.topBlock, 'Media > Top > Title').data.data}</h1>
+								<h6>{theMeta(data.topBlock, 'Media > Top > Sub Title').data.data}</h6>
 							</div>
+							<div className="col-12 col-md-5"></div>
 						</div>
-					</section>
-					<section className="ca_media_content ca_wave_bottom_inverted">
-						<div className="container">
-							<div className="row">
-								<div className="col-12 col-md-3">
-									<h3>Categories</h3>
-									<ul>
-										{cats.map((el, num) => {
-											return <li key={num}><a href="#" className={el.node.title.title === this.state.mediaFilter.value ? "ca_selected" : ""} onClick={(e) => this.categoryChange(el.node.title.title, e)}>{el.node.title.title}</a></li>
-										})}
-									</ul>
-								</div>
-								<div className="col-12 col-md-9">
-									<div className="row">
-										{media.map((el, num) => {
-											return (
-												<div key={num} className="col-12 col-md-4">
-													<div className="ca_img">
-														<img src={el.node.image === null ? "/img/media/photo.png" : el.node.image.file.url} alt="Kleros" />
-													</div>
-													<div className="ca_txt">
-														<div className="ca_date">{new Date(el.node.date).toLocaleDateString("en-US", {month: "long", day: "numeric", year: "numeric"})}</div>
-														<p>{el.node.title.title.split(" ").splice(0,20).join(" ")}</p>
-														<a target="_blank" href={el.node.link}>{el.node.linkText}</a>
-													</div>
+					</div>
+				</section>
+				<section className="ca_media_content ca_wave_bottom_inverted">
+					<div className="container">
+						<div className="row">
+							<div className="col-12 col-md-3">
+								<h3>Categories</h3>
+								<ul>
+									{cats.map((el, num) => {
+										return <li key={num}><a href="#" className={el.node.title.title === this.state.mediaFilter.value ? "ca_selected" : ""} onClick={(e) => this.categoryChange(el.node.title.title, e)}>{el.node.title.title}</a></li>
+									})}
+								</ul>
+							</div>
+							<div className="col-12 col-md-9">
+								<div className="row">
+									{media.map((el, num) => {
+										return (
+											<div key={num} className="col-12 col-md-4">
+												<div className="ca_img">
+													<img src={el.node.image === null ? "/img/media/photo.png" : el.node.image.file.url} alt="Kleros" />
 												</div>
-											)
-										})}
-									</div>
+												<div className="ca_txt">
+													<div className="ca_date">{new Date(el.node.date).toLocaleDateString("en-US", {month: "long", day: "numeric", year: "numeric"})}</div>
+													<p>{el.node.title.title.split(" ").splice(0,20).join(" ")}</p>
+													<a target="_blank" href={el.node.link}>{el.node.linkText}</a>
+												</div>
+											</div>
+										)
+									})}
 								</div>
 							</div>
 						</div>
-					</section>
-					<section className="ca_media_cta">
-						<div className="container">
-							<div className="row">
-								<div className="col-12 col-md-1"></div>
-								<div className="col-12 col-md-10">
-									<h2 className="ca_underlined">{theMeta(data.bottomBlock, 'Media > Bottom > CTA Title').data.data}</h2>
-									<h6>{theMeta(data.bottomBlock, 'Media > Bottom > CTA Text').data.data}</h6>
-									<a target={"_" + theMeta(data.bottomBlock, 'Media > Bottom > Buttons > Left').referenceData[0].target} href={theMeta(data.bottomBlock, 'Media > Bottom > Buttons > Left').referenceData[0].url} className="ca_button ca_solid_blue">{theMeta(data.bottomBlock, 'Media > Bottom > Buttons > Left').referenceData[0].text.text}</a>
-									<a target={"_" + theMeta(data.bottomBlock, 'Media > Bottom > Buttons > Right').referenceData[0].target} href={theMeta(data.bottomBlock, 'Media > Bottom > Buttons > Right').referenceData[0].url} className="ca_button ca_transparent_blue">{theMeta(data.bottomBlock, 'Media > Bottom > Buttons > Right').referenceData[0].text.text}</a>
-								</div>
-								<div className="col-12 col-md-1"></div>
+					</div>
+				</section>
+				<section className="ca_media_cta">
+					<div className="container">
+						<div className="row">
+							<div className="col-12 col-md-1"></div>
+							<div className="col-12 col-md-10">
+								<h2 className="ca_underlined">{theMeta(data.bottomBlock, 'Media > Bottom > CTA Title').data.data}</h2>
+								<h6>{theMeta(data.bottomBlock, 'Media > Bottom > CTA Text').data.data}</h6>
+								<a target={"_" + theMeta(data.bottomBlock, 'Media > Bottom > Buttons > Left').referenceData[0].target} href={theMeta(data.bottomBlock, 'Media > Bottom > Buttons > Left').referenceData[0].url} className="ca_button ca_solid_blue">{theMeta(data.bottomBlock, 'Media > Bottom > Buttons > Left').referenceData[0].text.text}</a>
+								<a onClick={this.onOpenModal} className="ca_button ca_transparent_blue ca_button_book">{theMeta(data.bottomBlock, 'Media > Bottom > Buttons > Right').referenceData[0].text.text}</a>
 							</div>
+							<div className="col-12 col-md-1"></div>
 						</div>
-					</section>
+					</div>
+				</section>
 			</div>
 		)
 	}
