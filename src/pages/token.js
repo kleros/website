1895e6dd3bd370edae5e2ad804b82331c6326ef8
Ferit Tunçer <ferit@cryptolab.net>
2019-11-21 15:38:22 +0300

    fix(token): mobile view

--- src/pages/token.js
+++ src/pages/token.js
@@ -1,56 +1,87 @@
-import React from 'react'
-import get from 'lodash/get'
-import Helmet from 'react-helmet'
-import { navigateTo } from 'gatsby-link'
-import Modal from 'react-responsive-modal'
-import theMeta from '../js/helpers.js'
+import React from "react";
+import get from "lodash/get";
+import Helmet from "react-helmet";
+import { navigateTo } from "gatsby-link";
+import Modal from "react-responsive-modal";
+import theMeta from "../js/helpers.js";
 
 const encode = data => {
   return Object.keys(data)
-    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
-    .join('&')
-}
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+};
 
 class RootIndex extends React.Component {
   state = {
-    firstname: '',
-    lastname: '',
-    companyEmail: '',
-    companyName: '',
-    message: '',
+    firstname: "",
+    lastname: "",
+    companyEmail: "",
+    companyName: "",
+    message: "",
     open: false
-  }
+  };
+
+  contentCards = [
+    {
+      title: "Stake as a Juror",
+      content:
+        "First and foremost, PNK holders can stake their token in a Kleros court to indicate their availability to serve as jurors. The more they stake, the more chances they have of selection. Jurors stake their PNK in courts they have relevant skills for with a random number generator (RNG) selecting the final spots based on size of stake.",
+      src:
+        "https://images.ctfassets.net/di6jbl3521d8/2bU4jVrv9tC60LyKQVCkax/885925df1c04bf9eac9c3ec2f5ba9f76/w2b.png"
+    },
+    {
+      title: "PNK Protects Against Sybil Attacks",
+      content:
+        "PNK is a protection against Sybil attacks. In order for an attacker to flood the juror pool, they need to buy enough PNK so that they are selected enough times to be a juror for the same case in order to change the outcome. Using a native token like PNK offers several key advantages for minimizing the risk of 51% attacks versus using an external cryptocurrency like ETH.",
+      src:
+        "https://images.ctfassets.net/di6jbl3521d8/4ayFRjrt8q7rzwyVlzgpwY/9e6636beb8dd884b4cce200be09386b0/w3.png"
+    },
+    {
+      title: "PNK Makes an Attack Expensive",
+      content:
+        "Imagine that someone does buy 51% of the PNK in an effort to attack Kleros. Maybe their attack will be subtle and go unnoticed. However, it’s more likely the community will realize that it is under attack. In this case, Kleros would lose credibility as an arbitration platform and the value of PNK would decrease. Then the attacker would take a substantial loss on PNK bought, representing a high economic attack cost.",
+      src:
+        "https://images.ctfassets.net/di6jbl3521d8/2k8nvzKwHXw9IY3TWZdMVz/415a24a0ab04934795da0a13aef450ba/w4.png"
+    },
+    {
+      title: "User Oriented",
+      content:
+        "Users have an interest in holding PNK tokens in order to gain voting rewards when taking part as jurors. They also have the opportunity to vote with their PNK on all Kleros Governance related proposals and votes creating an open and unbiased platform for all.",
+      src:
+        "https://images.ctfassets.net/di6jbl3521d8/Z3U68atBbDX6fGcxFqnWX/4a2ddcafeb24fd4d54bc38151aa0cace/w5b.png"
+    }
+  ];
 
   onCloseModal = () => {
-    this.setState({ open: false })
-  }
+    this.setState({ open: false });
+  };
 
   handleSubmit = e => {
-    e.preventDefault()
-    const form = e.target
+    e.preventDefault();
+    const form = e.target;
 
-    fetch('/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode({
-        'form-name': form.getAttribute('name'),
+        "form-name": form.getAttribute("name"),
         ...this.state
       })
     })
       .then(() =>
         this.setState({
-          firstname: '',
-          lastname: '',
-          companyEmail: '',
-          companyName: '',
-          message: '',
+          firstname: "",
+          lastname: "",
+          companyEmail: "",
+          companyName: "",
+          message: "",
           open: true
         })
       )
-      .catch(error => alert(error))
-  }
+      .catch(error => alert(error));
+  };
 
-  handleChange = e => this.setState({ [e.target.name]: e.target.value })
+  handleChange = e => this.setState({ [e.target.name]: e.target.value });
 
   render() {
     const {
@@ -60,19 +91,19 @@ class RootIndex extends React.Component {
       companyName,
       message,
       open
-    } = this.state
+    } = this.state;
 
-    const siteTitle = 'Kleros - Token Page'
-    const data = get(this, 'props.data.allContentfulTokenPage.edges')[0].node
-    console.log(data)
-    const top = data.top
-    console.log(theMeta(top, 'Token Page Top Button'))
+    const siteTitle = "Kleros - Token Page";
+    const data = get(this, "props.data.allContentfulTokenPage.edges")[0].node;
+    console.log(data);
+    const top = data.top;
+    console.log(theMeta(top, "Token Page Top Button"));
 
-    const escrow = data.escrow
-    const why = data.why
-    const contentCards = data.contentCards
-    const contentPath = data.contentPath
-    const contact = data.contact
+    const escrow = data.escrow;
+    const why = data.why;
+    const contentCards = this.contentCards;
+    const contentPath = data.contentPath;
+    const contact = data.contact;
 
     return (
       <div>
@@ -85,13 +116,13 @@ class RootIndex extends React.Component {
           onClose={this.onCloseModal}
           center
           classNames={{
-            modal: 'customModal'
+            modal: "customModal"
           }}
         >
           <h2>Message sent!</h2>
           <p>We will answer you as soon as possible.</p>
           <p>
-            While waiting for an answer you can join our{' '}
+            While waiting for an answer you can join our{" "}
             <a href="https://t.me/kleros" target="_blank">
               Telegram
             </a>
@@ -100,18 +131,18 @@ class RootIndex extends React.Component {
         </Modal>
         <section
           className="ca_home_top ca_escrow_top"
-          style={{ minHeight: '10vh' }}
+          style={{ minHeight: "10vh" }}
         >
           <div className="container">
             <div className="row ca_txt">
               <div className="col-12 col-md-6 text-center">
-                <h1 style={{ marginTop: '15%', fontSize: '420%' }}>
-                  {theMeta(top, 'Token Page Top Title').data.data}
+                <h1 style={{ marginTop: "15%", fontSize: "420%" }}>
+                  {theMeta(top, "Token Page Top Title").data.data}
                 </h1>
               </div>
               <div className="col-12 col-md-6">
                 <img
-                  src={theMeta(top, 'Token Page Image').mediaData[0].file.url}
+                  src={theMeta(top, "Token Page Image").mediaData[0].file.url}
                   className="illustration_escrow"
                   alt="Kleros"
                 />
@@ -122,58 +153,55 @@ class RootIndex extends React.Component {
 
         <section
           className="ca_escrow_content ca_wave_top_inverted"
-          style={{ paddingTop: '26rem' }}
+          style={{ paddingTop: "26rem" }}
         >
           <div className="container">
             {contentCards.map((el, num) => {
               return (
                 <div
                   key={num}
-                  className={'row ' + (num % 2 == 0 ? 'ca_left' : 'ca_right')}
+                  className={"row " + (num % 2 == 0 ? "ca_left" : "ca_right")}
                 >
                   <div className="col-12 col-md-1 one" />
                   <div className="col-12 col-md-5 two">
-                    <div
-                      dangerouslySetInnerHTML={{
-                        __html: el.text.childMarkdownRemark.html
-                      }}
-                    />
+                    <h2>{el.title}</h2>
+                    <p>{el.content}</p>
                   </div>
                   <div className="col-12 col-md-5 three">
                     <img
-                      style={{ width: '100%', marginLeft: '0' }}
-                      src={el.image.file.url}
+                      style={{ width: "100%", marginLeft: "0" }}
+                      src={el.src}
                       alt="Kleros"
                     />
                   </div>
                   <div className="col-12 col-md-1 four" />
                 </div>
-              )
+              );
             })}
           </div>
         </section>
 
         <section className="ca_escrow_contact">
-          <div className="container" style={{ height: '200px' }}>
+          <div className="container" style={{ height: "200px" }}>
             <div className="row d-flex justify-content-center">
               <div
                 className="card w-100 "
                 style={{
-                  borderRadius: '12px',
+                  borderRadius: "12px",
                   background:
-                    'linear-gradient(111.31deg, #4d00b4 19.55%, #6500b4 40.51%)',
-                  marginTop: '4%'
+                    "linear-gradient(111.31deg, #4d00b4 19.55%, #6500b4 40.51%)",
+                  marginTop: "4%"
                 }}
               >
-                <div className="card-body" style={{ color: 'white' }}>
-                  <div className="row" style={{ alignItems: 'center' }}>
+                <div className="card-body" style={{ color: "white" }}>
+                  <div className="row" style={{ alignItems: "center" }}>
                     <div className="col-12 col-md-1">
                       <img src="/img/stake_icon.svg" />
                     </div>
                     <div className="col-12 col-md-8">
                       <h3
                         className="card-title"
-                        style={{ lineHeight: 'normal', marginBottom: '0' }}
+                        style={{ lineHeight: "normal", marginBottom: "0" }}
                       >
                         If you are interested in acquiring PNK token OTC, get in
                         touch
@@ -181,7 +209,7 @@ class RootIndex extends React.Component {
 
                       <p
                         className="card-text ca-text"
-                        style={{ fontSize: '130%' }}
+                        style={{ fontSize: "130%" }}
                       >
                         Note that in order to ensure fairness in token
                         distribution, tokens are sold to buyers at prices
@@ -203,11 +231,11 @@ class RootIndex extends React.Component {
           </div>
         </section>
       </div>
-    )
+    );
   }
 }
 
-export default RootIndex
+export default RootIndex;
 
 export const pageQuery = graphql`
   query tokenQuery {
@@ -387,4 +415,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
