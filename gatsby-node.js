7d9f75385d07d80486ca157390ac05031161b315
Ferit Tunçer <ferit@cryptolab.net>
2019-12-21 20:44:28 +0300

    refactor(gatsby-node): remove unnecessary createPages implementation

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,50 +1,51 @@
-const Promise = require('bluebird')
-const path = require('path')
+const Promise = require("bluebird");
+const path = require("path");
 
-const axios = require('axios')
-const crypto = require('crypto')
+const axios = require("axios");
+const crypto = require("crypto");
 
 exports.sourceNodes = async ({ boundActionCreators }) => {
-  const { createNode } = boundActionCreators
+  const { createNode } = boundActionCreators;
 
   // fetch raw data from the apis
-  const fetchKlerosForum = () => axios.get('https://forum.kleros.io/posts.json')
+  const fetchKlerosForum = () =>
+    axios.get("https://forum.kleros.io/posts.json");
 
-  const gitToken = '7e38f573a0b8f79d6a574e6de6c8f8742ea2509b'
-  const gitOwner = 'kleros'
-  const gitBranch = 'master'
+  const gitToken = "7e38f573a0b8f79d6a574e6de6c8f8742ea2509b";
+  const gitOwner = "kleros";
+  const gitBranch = "master";
   const fetchKlerosRepositories = () =>
-    axios.get('https://api.github.com/orgs/kleros/repos?sort=updated')
+    axios.get("https://api.github.com/orgs/kleros/repos?sort=updated");
   const fetchKlerosGit = gitRepo =>
     axios.get(
-      'https://api.github.com/repos/' +
+      "https://api.github.com/repos/" +
         gitOwner +
-        '/' +
+        "/" +
         gitRepo +
-        '/commits/' +
+        "/commits/" +
         gitBranch +
-        '?access_token=' +
+        "?access_token=" +
         gitToken
-    )
+    );
 
-  const blogClientID = 'ghost-frontend'
-  const blogClientSecret = '1f107d706c2a'
+  const blogClientID = "ghost-frontend";
+  const blogClientSecret = "1f107d706c2a";
   const fetchKlerosBlog = () =>
     axios.get(
-      'https://blog.kleros.io/ghost/api/v0.1/posts?client_id=' +
+      "https://blog.kleros.io/ghost/api/v0.1/posts?client_id=" +
         blogClientID +
-        '&client_secret=' +
+        "&client_secret=" +
         blogClientSecret
-    )
+    );
 
   // await for results
-  const forum = await fetchKlerosForum()
-  const blog = await fetchKlerosBlog()
-  const repositories = await fetchKlerosRepositories()
+  const forum = await fetchKlerosForum();
+  const blog = await fetchKlerosBlog();
+  const repositories = await fetchKlerosRepositories();
 
   const git = await Promise.all(
     repositories.data.map(async repo => await fetchKlerosGit(repo.name))
-  )
+  );
 
   // map into these results and create nodes
   blog.data.posts.map((blog_post, i) => {
@@ -59,16 +60,16 @@ exports.sourceNodes = async ({ boundActionCreators }) => {
       post_title: blog_post.title,
       post_date: blog_post.published_at,
       post_url: blog_post.url
-    }
+    };
 
     var contentDigest = crypto
       .createHash(`md5`)
       .update(JSON.stringify(caNode))
-      .digest(`hex`)
-    caNode.internal.contentDigest = contentDigest
+      .digest(`hex`);
+    caNode.internal.contentDigest = contentDigest;
 
-    createNode(caNode)
-  })
+    createNode(caNode);
+  });
   git
     .map(response => response.data)
     .map((commit, i) => {
@@ -83,16 +84,16 @@ exports.sourceNodes = async ({ boundActionCreators }) => {
         commit_text: commit.commit.message,
         commit_date: commit.commit.committer.date,
         commit_url: commit.html_url
-      }
+      };
 
       var contentDigest = crypto
         .createHash(`md5`)
         .update(JSON.stringify(caNode))
-        .digest(`hex`)
-      caNode.internal.contentDigest = contentDigest
+        .digest(`hex`);
+      caNode.internal.contentDigest = contentDigest;
 
-      createNode(caNode)
-    })
+      createNode(caNode);
+    });
   forum.data.latest_posts.map((forum_posts, i) => {
     // Create your node object
     var caNode = {
@@ -112,61 +113,19 @@ exports.sourceNodes = async ({ boundActionCreators }) => {
       topic_id: forum_posts.topic_id,
       topic_slug: forum_posts.topic_slug,
       topic_title: forum_posts.topic_title
-    }
+    };
 
     // Get content digest of node. (Required field)
     var contentDigest = crypto
       .createHash(`md5`)
       .update(JSON.stringify(caNode))
-      .digest(`hex`)
+      .digest(`hex`);
     // add it to caNode
-    caNode.internal.contentDigest = contentDigest
+    caNode.internal.contentDigest = contentDigest;
 
     // Create node with the gatsby createNode() API
-    createNode(caNode)
-  })
+    createNode(caNode);
+  });
 
-  return
-}
-
-exports.createPages = ({ graphql, boundActionCreators }) => {
-  const { createPage } = boundActionCreators
-
-  return new Promise((resolve, reject) => {
-    const blogPost = path.resolve('./src/templates/page.js')
-    resolve(
-      graphql(
-        `
-          {
-            allContentfulPage(sort: { fields: [date, slug], order: DESC }) {
-              edges {
-                node {
-                  title {
-                    title
-                  }
-                  slug
-                }
-              }
-            }
-          }
-        `
-      ).then(result => {
-        if (result.errors) {
-          console.log(result.errors)
-          reject(result.errors)
-        }
-
-        const pages = result.data.allContentfulPage.edges
-        pages.forEach((page, index) => {
-          createPage({
-            path: `/${page.node.slug}/`,
-            component: blogPost,
-            context: {
-              slug: page.node.slug
-            }
-          })
-        })
-      })
-    )
-  })
-}
+  return;
+};
