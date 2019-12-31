9efb8181c9ba91090c1872414a908434ea7f941d
Ferit Tunçer <ferit@cryptolab.net>
2019-12-31 03:10:24 +0300

    fix(escrow): typo

--- src/pages/escrow.js
+++ src/pages/escrow.js
@@ -343,7 +343,6 @@ class RootIndex extends React.Component {
                     </div>
                   </div>
                 </div>
-                ); })}
               </div>
               <div className="col-12 col-md-2"></div>
             </div>
