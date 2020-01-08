292384e9240c0f64b9f7a255bf81a337e5b92682
Ferit Tunçer <ferit@cryptolab.net>
2020-01-08 15:18:33 +0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
         // language JSON resource path
         path: `${__dirname}/src/intl`,
         // supported language
-        languages: [`en`, `ko`, `es`, `ru`, `fr`, "tr", "pt"],
+        languages: [`en`, `ko`, `es`, `ru`, `fr`, "tr", "pt", "zh"],
         // language file path
         defaultLanguage: `en`,
         // option to redirect to `/en` when connecting `/`
