183c8fb74f18534b523dd5a1ebf73c50de0f3b19
Ferit Tunçer <ferit@cryptolab.net>
2020-01-10 00:24:40 +0300

    fix(hosted-by): fix responsiveness

--- src/components/landing/hosted-by.js
+++ src/components/landing/hosted-by.js
@@ -5,11 +5,11 @@ import ThomsonReuters from "../../../static/assets/images/thomson-reuters.png";
 const HostedByBar = styled.div`
   background: #1e075f;
   width: 100%;
-  height: 175px;
   text-align: center;
 `;
 const StyledThomsonReuters = styled.img`
-  margin-top: 55px;
+  margin-top: 4rem;
+  margin-bottom: 4rem;
   width: 50%;
   position: relative;
 
