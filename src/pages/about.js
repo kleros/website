dec03bb8b9447f4aee745599fcc9bbb96b5ac776
Ferit Tunçer <ferit@cryptolab.net>
2020-01-18 09:44:37 +0300

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -82,7 +82,8 @@ class RootIndex extends React.Component {
                     {
                       breakpoint: 768,
                       settings: {
-                        slidesToShow: 1
+                        slidesToShow: 1, 
+                        slidesToScroll: 1
                       }
                     }
                   ]}
