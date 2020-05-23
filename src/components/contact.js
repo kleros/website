26a4a9d06c739954caa7a05a705e2daa68ca7d28
Ferit Tunçer <ferit@cryptolab.net>
2020-05-23 16:31:57 +0300

    refactor(contact): styling tweak

--- src/components/contact.js
+++ src/components/contact.js
@@ -12,16 +12,18 @@ import styles from "./styles/contact.module.css";
 const Contact = ({ intl }) => (
   <div className={styles.contact}>
     <Logo />
-    <span className="text">
-      <FormattedMessage id="contact.title" />
-    </span>
-    <div>
-      <a href="https://t.me/kleros" target="blank" rel="noopener noreferrer">
-        <img src={telegram} alt="telegram icon" />
-      </a>
-      <a href="mailto:contract@kleros.io" target="blank" rel="noopener noreferrer">
-        <img src={email} alt="email icon" />
-      </a>
+    <div className={styles.rest}>
+      <span className="text">
+        <FormattedMessage id="contact.title" />
+      </span>
+      <div className={styles.icons}>
+        <a href="https://t.me/kleros" target="blank" rel="noopener noreferrer">
+          <img src={telegram} alt="telegram icon" />
+        </a>
+        <a href="mailto:contract@kleros.io" target="blank" rel="noopener noreferrer">
+          <img src={email} alt="email icon" />
+        </a>
+      </div>
     </div>
   </div>
 );
