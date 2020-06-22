10c10873d4c27caf63c18be258de834b6eeaffaf
Ferit Tunçer <ferit@cryptolab.net>
2020-06-22 21:21:31 +0300

    feat(community): intl en done

--- src/pages/community.js
+++ src/pages/community.js
@@ -4,14 +4,15 @@ import Layout from "../components/layout";
 import Image from "../components/image";
 import SEO from "../components/seo";
 
-const IndexPage = () => (
+import { injectIntl, Link, FormattedMessage } from "gatsby-plugin-intl";
+
+import styles from "./styles/community.module.css";
+
+const Community = () => (
   <Layout>
-    <SEO title="Home" />
-    <p>Not Implemented Yet</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <SEO title="Community" />
+    <div className={styles.community}>asda</div>
   </Layout>
 );
 
-export default IndexPage;
+export default injectIntl(Community);
