d6187ac2037f94b27d12fb201f063f6767deb99d
Ferit Tunçer <ferit@cryptolab.net>
2020-07-07 00:05:34 +0300

    anchor hrefs for some buttons

--- src/pages/curate.js
+++ src/pages/curate.js
@@ -1,183 +1,183 @@
-import React from "react";
-import Layout from "../components/layout";
-import SEO from "../components/seo";
-import Sash from "../components/sash";
-import HorizontalCard from "../components/horizontal-card";
-import VerticalCard from "../components/vertical-card";
-import Sponsors from "../components/sponsors";
-import Contact from "../components/contact";
-import Deversifi from "src/assets/images/deversifi.png";
-import Curate1 from "src/assets/images/curate1.png";
-import Curate2 from "src/assets/images/curate2.png";
-import CurateTwoScreen from "src/assets/svgs/curate-twoscreen.svg";
-import CurateHero from "src/assets/images/curate-hero.png";
-import List from "src/assets/svgs/icon-list.svg";
-import TCR from "src/assets/svgs/icon-tcr.svg";
-import Scales from "src/assets/svgs/icon-scales.svg";
-import copyright from "src/assets/svgs/icon-copyright.svg";
-import eCommerce from "src/assets/svgs/icon-e-commerce.svg";
-import finance from "src/assets/svgs/icon-finance.svg";
-import freelancing from "src/assets/svgs/icon-freelance.svg";
-import insurance from "src/assets/svgs/icon-shield.svg";
-import moderation from "src/assets/svgs/icon-moderation.svg";
-import plus from "src/assets/svgs/icon-plus.svg";
-import smallClaims from "src/assets/svgs/icon-gavel.svg";
-import token from "src/assets/svgs/icon-token.svg";
-import restaurant from "src/assets/svgs/icon-fork-n-knife.svg";
-import hotel from "src/assets/svgs/icon-hotel.svg";
-import spam from "src/assets/svgs/icon-spam.svg";
-import social from "src/assets/svgs/icon-speaker.svg";
-import bounty from "src/assets/svgs/icon-bounty.svg";
-import otc from "src/assets/svgs/icon-otc.svg";
-import crowdfunding from "src/assets/svgs/icon-crowdfunding.svg";
-import payroll from "src/assets/svgs/icon-payroll.svg";
-import www from "src/assets/svgs/icon-www.svg";
-import Ninja from "src/assets/svgs/ninja-black-text.svg";
-import { Badge, Col, Container, Row } from "react-bootstrap";
-import { FormattedMessage, Link, injectIntl } from "gatsby-plugin-intl";
-import styles from "./styles/curate.module.css";
+import React from 'react'
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import Sash from '../components/sash'
+import HorizontalCard from '../components/horizontal-card'
+import VerticalCard from '../components/vertical-card'
+import Sponsors from '../components/sponsors'
+import Contact from '../components/contact'
+import Deversifi from 'src/assets/images/deversifi.png'
+import Curate1 from 'src/assets/images/curate1.png'
+import Curate2 from 'src/assets/images/curate2.png'
+import CurateTwoScreen from 'src/assets/svgs/curate-twoscreen.svg'
+import CurateHero from 'src/assets/images/curate-hero.png'
+import List from 'src/assets/svgs/icon-list.svg'
+import TCR from 'src/assets/svgs/icon-tcr.svg'
+import Scales from 'src/assets/svgs/icon-scales.svg'
+import copyright from 'src/assets/svgs/icon-copyright.svg'
+import eCommerce from 'src/assets/svgs/icon-e-commerce.svg'
+import finance from 'src/assets/svgs/icon-finance.svg'
+import freelancing from 'src/assets/svgs/icon-freelance.svg'
+import insurance from 'src/assets/svgs/icon-shield.svg'
+import moderation from 'src/assets/svgs/icon-moderation.svg'
+import plus from 'src/assets/svgs/icon-plus.svg'
+import smallClaims from 'src/assets/svgs/icon-gavel.svg'
+import token from 'src/assets/svgs/icon-token.svg'
+import restaurant from 'src/assets/svgs/icon-fork-n-knife.svg'
+import hotel from 'src/assets/svgs/icon-hotel.svg'
+import spam from 'src/assets/svgs/icon-spam.svg'
+import social from 'src/assets/svgs/icon-speaker.svg'
+import bounty from 'src/assets/svgs/icon-bounty.svg'
+import otc from 'src/assets/svgs/icon-otc.svg'
+import crowdfunding from 'src/assets/svgs/icon-crowdfunding.svg'
+import payroll from 'src/assets/svgs/icon-payroll.svg'
+import www from 'src/assets/svgs/icon-www.svg'
+import Ninja from 'src/assets/svgs/ninja-black-text.svg'
+import { Badge, Col, Container, Row } from 'react-bootstrap'
+import { FormattedMessage, Link, injectIntl } from 'gatsby-plugin-intl'
+import styles from './styles/curate.module.css'
 
 const Curate = ({ intl }) => (
   <Layout>
-    <SEO lang={intl.locale} title="Curate" />
+    <SEO lang={intl.locale} title='Curate' />
     <Container className={styles.curate} fluid>
       <section className={styles.hero}>
         <h1>
-          <FormattedMessage id="curate.section-hero.h1" />
+          <FormattedMessage id='curate.section-hero.h1' />
         </h1>
         <h2>
-          <FormattedMessage id="curate.section-hero.h2" />
+          <FormattedMessage id='curate.section-hero.h2' />
         </h2>
 
         <Container className={styles.buttonWrapper}>
-          <a className="btn btn-primary" href="https://curate.kleros.io" rel="noopener noreferrer" target="blank">
-            <FormattedMessage id="curate.section-hero.button-primary" />
+          <a className='btn btn-primary' href='https://curate.kleros.io' rel='noopener noreferrer' target='blank'>
+            <FormattedMessage id='curate.section-hero.button-primary' />
           </a>
         </Container>
         <img alt={CurateHero} src={CurateHero} />
       </section>
-      <section className="light">
+      <section className='light'>
         <Sash
-          as="div"
+          as='div'
           figures={[
             {
               icon: List,
               text: intl.formatMessage({
-                id: "curate.sash.first.paragraph",
+                id: 'curate.sash.first.paragraph'
               }),
               title: intl.formatMessage({
-                id: "curate.sash.first.title",
-              }),
+                id: 'curate.sash.first.title'
+              })
             },
             {
               icon: TCR,
               text: intl.formatMessage({
-                id: "curate.sash.second.paragraph",
+                id: 'curate.sash.second.paragraph'
               }),
               title: intl.formatMessage({
-                id: "curate.sash.second.title",
-              }),
+                id: 'curate.sash.second.title'
+              })
             },
             {
               icon: Scales,
               text: intl.formatMessage({
-                id: "curate.sash.third.paragraph",
+                id: 'curate.sash.third.paragraph'
               }),
               title: intl.formatMessage({
-                id: "curate.sash.third.title",
-              }),
-            },
+                id: 'curate.sash.third.title'
+              })
+            }
           ]}
         />
       </section>
       <section className={styles.usecases}>
-        <h1>{intl.formatMessage({ id: "curate.section-use-case.h1" })}</h1>
+        <h1>{intl.formatMessage({ id: 'curate.section-use-case.h1' })}</h1>
         <div className={styles.cards}>
           <HorizontalCard
             content={{
               icon: token,
-              text: intl.formatMessage({ id: "curate.section-use-case.cards.1" }),
+              text: intl.formatMessage({ id: 'curate.section-use-case.cards.1' })
             }}
           />
           <HorizontalCard
             content={{
               icon: moderation,
-              text: intl.formatMessage({ id: "curate.section-use-case.cards.2" }),
+              text: intl.formatMessage({ id: 'curate.section-use-case.cards.2' })
             }}
-          />{" "}
+          />{' '}
           <HorizontalCard
             content={{
               icon: hotel,
-              text: intl.formatMessage({ id: "curate.section-use-case.cards.3" }),
+              text: intl.formatMessage({ id: 'curate.section-use-case.cards.3' })
             }}
-          />{" "}
+          />{' '}
           <HorizontalCard
             content={{
               icon: restaurant,
-              text: intl.formatMessage({ id: "curate.section-use-case.cards.4" }),
+              text: intl.formatMessage({ id: 'curate.section-use-case.cards.4' })
             }}
-          />{" "}
+          />{' '}
           <HorizontalCard
             content={{
               icon: insurance,
-              text: intl.formatMessage({ id: "curate.section-use-case.cards.5" }),
+              text: intl.formatMessage({ id: 'curate.section-use-case.cards.5' })
             }}
-          />{" "}
+          />{' '}
           <HorizontalCard
             content={{
               icon: eCommerce,
-              text: intl.formatMessage({ id: "curate.section-use-case.cards.6" }),
+              text: intl.formatMessage({ id: 'curate.section-use-case.cards.6' })
             }}
-          />{" "}
+          />{' '}
           <HorizontalCard
             content={{
               icon: social,
-              text: intl.formatMessage({ id: "curate.section-use-case.cards.7" }),
+              text: intl.formatMessage({ id: 'curate.section-use-case.cards.7' })
             }}
-          />{" "}
+          />{' '}
           <HorizontalCard
             content={{
               icon: spam,
-              text: intl.formatMessage({ id: "curate.section-use-case.cards.8" }),
+              text: intl.formatMessage({ id: 'curate.section-use-case.cards.8' })
             }}
-          />{" "}
+          />{' '}
           <HorizontalCard
             content={{
               icon: plus,
-              text: intl.formatMessage({ id: "curate.section-use-case.cards.9" }),
+              text: intl.formatMessage({ id: 'curate.section-use-case.cards.9' })
             }}
           />
         </div>
       </section>
 
-      <section className="masked">
-        <span className="trapezium" />
+      <section className='masked'>
+        <span className='trapezium' />
         <h1>
-          <FormattedMessage id="curate.section-3.h1" />
+          <FormattedMessage id='curate.section-3.h1' />
         </h1>
         <h2>
-          <FormattedMessage id="curate.section-3.h2" />
+          <FormattedMessage id='curate.section-3.h2' />
         </h2>
-        <a className="btn btn-secondary">
-          <FormattedMessage id="curate.section-3.button" />
+        <a href='https://curate.kleros.io' target='blank' rel='noopener noreferrer' className='btn btn-secondary'>
+          <FormattedMessage id='curate.section-3.button' />
         </a>
       </section>
       <section>
         <h1>
-          <FormattedMessage id="curate.section-4.h1" />
+          <FormattedMessage id='curate.section-4.h1' />
         </h1>
-        <Container className="mt-5 d-flex flex-wrap justify-content-around">
-          <Badge as="a" className={styles.badge} href="https://www.deversifi.com/">
-            <span class={styles.helper}></span>
+        <Container className='mt-5 d-flex flex-wrap justify-content-around'>
+          <Badge as='a' className={styles.badge} href='https://www.deversifi.com/'>
+            <span class={styles.helper} />
             <img src={Deversifi} />
           </Badge>
-          <Badge as="a" className={styles.badge} href="https://uniswap.ninja/">
+          <Badge as='a' className={styles.badge} href='https://uniswap.ninja/'>
             <Ninja />
           </Badge>
         </Container>
       </section>
-      <div></div>
-      <section className="nopadding">
+      <div />
+      <section className='nopadding'>
         <CurateTwoScreen />
       </section>
       <hr />
@@ -185,13 +185,13 @@ const Curate = ({ intl }) => (
         <Contact
           content={{
             title: intl.formatMessage({
-              id: "contact.title",
-            }),
+              id: 'contact.title'
+            })
           }}
         />
       </section>
     </Container>
   </Layout>
-);
+)
 
-export default injectIntl(Curate);
+export default injectIntl(Curate)
--- src/pages/escrow.js
+++ src/pages/escrow.js
@@ -1,181 +1,181 @@
-import React from "react";
-import Layout from "../components/layout";
-import SEO from "../components/seo";
-import Sash from "../components/sash";
-import HorizontalCard from "../components/horizontal-card";
-import VerticalCard from "../components/vertical-card";
-import Sponsors from "../components/sponsors";
-import Contact from "../components/contact";
-import Deversifi from "src/assets/images/deversifi.png";
-import Escrow1 from "src/assets/svgs/escrow-screen1.svg";
-import Escrow2 from "src/assets/svgs/escrow-screen2.svg";
-import EscrowTwoScreen from "src/assets/svgs/escrow-twoscreens.svg";
-import EscrowHero from "src/assets/svgs/escrow-hero.svg";
-import Warning from "src/assets/svgs/icon-warning.svg";
-import EscrowIcon from "src/assets/svgs/icon-escrow.svg";
-import Kleros from "src/assets/svgs/icon-kleros.svg";
-import copyright from "src/assets/svgs/icon-copyright.svg";
-import eCommerce from "src/assets/svgs/icon-e-commerce.svg";
-import finance from "src/assets/svgs/icon-finance.svg";
-import freelancing from "src/assets/svgs/icon-freelance.svg";
-import insurance from "src/assets/svgs/icon-shield.svg";
-import moderation from "src/assets/svgs/icon-moderation.svg";
-import plus from "src/assets/svgs/icon-plus.svg";
-import smallClaims from "src/assets/svgs/icon-gavel.svg";
-import token from "src/assets/svgs/icon-token.svg";
-import restaurant from "src/assets/svgs/icon-fork-n-knife.svg";
-import hotel from "src/assets/svgs/icon-hotel.svg";
-import spam from "src/assets/svgs/icon-spam.svg";
-import social from "src/assets/svgs/icon-speaker.svg";
-import bounty from "src/assets/svgs/icon-bounty.svg";
-import otc from "src/assets/svgs/icon-otc.svg";
-import crowdfunding from "src/assets/svgs/icon-crowdfunding.svg";
-import payroll from "src/assets/svgs/icon-payroll.svg";
-import www from "src/assets/svgs/icon-www.svg";
-import Ninja from "src/assets/svgs/ninja-black-text.svg";
-import { Badge, Col, Container, Row } from "react-bootstrap";
-import { FormattedMessage, Link, injectIntl } from "gatsby-plugin-intl";
-import styles from "./styles/escrow.module.css";
+import React from 'react'
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import Sash from '../components/sash'
+import HorizontalCard from '../components/horizontal-card'
+import VerticalCard from '../components/vertical-card'
+import Sponsors from '../components/sponsors'
+import Contact from '../components/contact'
+import Deversifi from 'src/assets/images/deversifi.png'
+import Escrow1 from 'src/assets/svgs/escrow-screen1.svg'
+import Escrow2 from 'src/assets/svgs/escrow-screen2.svg'
+import EscrowTwoScreen from 'src/assets/svgs/escrow-twoscreens.svg'
+import EscrowHero from 'src/assets/svgs/escrow-hero.svg'
+import Warning from 'src/assets/svgs/icon-warning.svg'
+import EscrowIcon from 'src/assets/svgs/icon-escrow.svg'
+import Kleros from 'src/assets/svgs/icon-kleros.svg'
+import copyright from 'src/assets/svgs/icon-copyright.svg'
+import eCommerce from 'src/assets/svgs/icon-e-commerce.svg'
+import finance from 'src/assets/svgs/icon-finance.svg'
+import freelancing from 'src/assets/svgs/icon-freelance.svg'
+import insurance from 'src/assets/svgs/icon-shield.svg'
+import moderation from 'src/assets/svgs/icon-moderation.svg'
+import plus from 'src/assets/svgs/icon-plus.svg'
+import smallClaims from 'src/assets/svgs/icon-gavel.svg'
+import token from 'src/assets/svgs/icon-token.svg'
+import restaurant from 'src/assets/svgs/icon-fork-n-knife.svg'
+import hotel from 'src/assets/svgs/icon-hotel.svg'
+import spam from 'src/assets/svgs/icon-spam.svg'
+import social from 'src/assets/svgs/icon-speaker.svg'
+import bounty from 'src/assets/svgs/icon-bounty.svg'
+import otc from 'src/assets/svgs/icon-otc.svg'
+import crowdfunding from 'src/assets/svgs/icon-crowdfunding.svg'
+import payroll from 'src/assets/svgs/icon-payroll.svg'
+import www from 'src/assets/svgs/icon-www.svg'
+import Ninja from 'src/assets/svgs/ninja-black-text.svg'
+import { Badge, Col, Container, Row } from 'react-bootstrap'
+import { FormattedMessage, Link, injectIntl } from 'gatsby-plugin-intl'
+import styles from './styles/escrow.module.css'
 
 const Escrow = ({ intl }) => (
   <Layout>
-    <SEO lang={intl.locale} title="Escrow" />
+    <SEO lang={intl.locale} title='Escrow' />
     <Container className={styles.escrow} fluid>
       <section className={styles.hero}>
         <h1>
-          <FormattedMessage id="escrow.section-hero.h1" />
+          <FormattedMessage id='escrow.section-hero.h1' />
         </h1>
         <h2>
-          <FormattedMessage id="escrow.section-hero.h2" />
+          <FormattedMessage id='escrow.section-hero.h2' />
         </h2>
 
         <Container className={styles.buttonWrapper}>
-          <a className="btn btn-primary" href="https://escrow.kleros.io" rel="noopener noreferrer" target="blank">
-            <FormattedMessage id="escrow.section-hero.button-primary" />
+          <a className='btn btn-primary' href='https://escrow.kleros.io' rel='noopener noreferrer' target='blank'>
+            <FormattedMessage id='escrow.section-hero.button-primary' />
           </a>
         </Container>
         <EscrowHero />
       </section>
-      <section className="light">
+      <section className='light'>
         <Sash
-          as="div"
+          as='div'
           figures={[
             {
               icon: Warning,
               text: intl.formatMessage({
-                id: "escrow.sash.first.paragraph",
+                id: 'escrow.sash.first.paragraph'
               }),
               title: intl.formatMessage({
-                id: "escrow.sash.first.title",
-              }),
+                id: 'escrow.sash.first.title'
+              })
             },
             {
               icon: EscrowIcon,
               text: intl.formatMessage({
-                id: "escrow.sash.second.paragraph",
+                id: 'escrow.sash.second.paragraph'
               }),
               title: intl.formatMessage({
-                id: "escrow.sash.second.title",
-              }),
+                id: 'escrow.sash.second.title'
+              })
             },
             {
               icon: Kleros,
               text: intl.formatMessage({
-                id: "escrow.sash.third.paragraph",
+                id: 'escrow.sash.third.paragraph'
               }),
               title: intl.formatMessage({
-                id: "escrow.sash.third.title",
-              }),
-            },
+                id: 'escrow.sash.third.title'
+              })
+            }
           ]}
         />
       </section>
 
       <section className={styles.usecases}>
-        <h1>{intl.formatMessage({ id: "escrow.section-use-case.h1" })}</h1>
+        <h1>{intl.formatMessage({ id: 'escrow.section-use-case.h1' })}</h1>
         <div className={styles.cards}>
           <HorizontalCard
             content={{
               icon: freelancing,
-              text: intl.formatMessage({ id: "escrow.section-use-case.cards.1" }),
+              text: intl.formatMessage({ id: 'escrow.section-use-case.cards.1' })
             }}
           />
           <HorizontalCard
             content={{
               icon: www,
-              text: intl.formatMessage({ id: "escrow.section-use-case.cards.2" }),
+              text: intl.formatMessage({ id: 'escrow.section-use-case.cards.2' })
             }}
-          />{" "}
+          />{' '}
           <HorizontalCard
             content={{
               icon: bounty,
-              text: intl.formatMessage({ id: "escrow.section-use-case.cards.3" }),
+              text: intl.formatMessage({ id: 'escrow.section-use-case.cards.3' })
             }}
-          />{" "}
+          />{' '}
           <HorizontalCard
             content={{
               icon: eCommerce,
-              text: intl.formatMessage({ id: "escrow.section-use-case.cards.4" }),
+              text: intl.formatMessage({ id: 'escrow.section-use-case.cards.4' })
             }}
-          />{" "}
+          />{' '}
           <HorizontalCard
             content={{
               icon: payroll,
-              text: intl.formatMessage({ id: "escrow.section-use-case.cards.5" }),
+              text: intl.formatMessage({ id: 'escrow.section-use-case.cards.5' })
             }}
-          />{" "}
+          />{' '}
           <HorizontalCard
             content={{
               icon: crowdfunding,
-              text: intl.formatMessage({ id: "escrow.section-use-case.cards.6" }),
+              text: intl.formatMessage({ id: 'escrow.section-use-case.cards.6' })
             }}
-          />{" "}
+          />{' '}
           <HorizontalCard
             content={{
               icon: otc,
-              text: intl.formatMessage({ id: "escrow.section-use-case.cards.7" }),
+              text: intl.formatMessage({ id: 'escrow.section-use-case.cards.7' })
             }}
-          />{" "}
+          />{' '}
           <HorizontalCard
             content={{
               icon: copyright,
-              text: intl.formatMessage({ id: "escrow.section-use-case.cards.8" }),
+              text: intl.formatMessage({ id: 'escrow.section-use-case.cards.8' })
             }}
-          />{" "}
+          />{' '}
           <HorizontalCard
             content={{
               icon: plus,
-              text: intl.formatMessage({ id: "escrow.section-use-case.cards.9" }),
+              text: intl.formatMessage({ id: 'escrow.section-use-case.cards.9' })
             }}
           />
         </div>
       </section>
 
-      <section className="masked">
-        <span className="trapezium" />
+      <section className='masked'>
+        <span className='trapezium' />
         <h1>
-          <FormattedMessage id="escrow.section-3.h1" />
+          <FormattedMessage id='escrow.section-3.h1' />
         </h1>
         <h2>
-          <FormattedMessage id="escrow.section-3.h2" />
+          <FormattedMessage id='escrow.section-3.h2' />
         </h2>
-        <a href="https://escrow.kleros.io" className="btn btn-secondary">
-          <FormattedMessage id="escrow.section-3.button" />
+        <a href='https://escrow.kleros.io' className='btn btn-secondary'>
+          <FormattedMessage id='escrow.section-3.button' />
         </a>
       </section>
       <section>
         <h1>
-          <FormattedMessage id="escrow.section-4.h1" />
+          <FormattedMessage id='escrow.section-4.h1' />
         </h1>
         <h2>
-          <FormattedMessage id="escrow.section-4.h2" />
+          <FormattedMessage id='escrow.section-4.h2' />
         </h2>
-        <a className="btn btn-primary">
-          <FormattedMessage id="escrow.section-4.button" />
-        </a>
+        <Link to='/integrations' className='btn btn-primary'>
+          <FormattedMessage id='escrow.section-4.button' />
+        </Link>
       </section>
       <div />
-      <section className="nopadding">
+      <section className='nopadding'>
         <EscrowTwoScreen />
       </section>
       <hr />
@@ -183,13 +183,13 @@ const Escrow = ({ intl }) => (
         <Contact
           content={{
             title: intl.formatMessage({
-              id: "contact.title",
-            }),
+              id: 'contact.title'
+            })
           }}
         />
       </section>
     </Container>
   </Layout>
-);
+)
 
-export default injectIntl(Escrow);
+export default injectIntl(Escrow)
