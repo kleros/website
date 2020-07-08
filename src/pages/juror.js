44291fc2a7c2e454e93ce7121d295bed57124c52
Ferit Tunçer <ferit@cryptolab.net>
2020-07-08 12:03:16 +0300

    make redirect work on every locale

--- src/pages/juror.js
+++ src/pages/juror.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-if (typeof window !== `undefined` && window.location.pathname.match(/\/en\/juror\/*/)) window.location.replace('https://blog.kleros.io/become-a-juror-blockchain-dispute-resolution-on-ethereum/'); // highlight-line
+if (typeof window !== `undefined` && window.location.pathname.match(/\/\w{2}\/juror\/*/)) window.location.replace('https://blog.kleros.io/become-a-juror-blockchain-dispute-resolution-on-ethereum/'); // highlight-line
 
 const Juror = () => <></>;
 
