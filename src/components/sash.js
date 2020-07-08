42345c310608362551b0174b5fa5093d39868c52
Ferit Tunçer <ferit@cryptolab.net>
2020-07-08 12:42:15 +0300

    refactor(research): add other researchers

--- src/components/sash.js
+++ src/components/sash.js
@@ -5,42 +5,47 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react";
-import styles from "./styles/sash.module.css";
-import { Col, Container, Row } from "react-bootstrap";
-import { injectIntl } from "gatsby-plugin-intl";
+import React from 'react'
+import styles from './styles/sash.module.css'
+import { Col, Container, Row } from 'react-bootstrap'
+import { injectIntl } from 'gatsby-plugin-intl'
 
 const Sash = ({ separator, figures, intl }) => (
   <Container className={styles.sash} fluid>
-    <div className="d-block d-lg-flex">
+    <div className='d-block d-lg-flex'>
       {figures.map((figure, index) => {
         const item = (
           <Col className={styles.column} key={index}>
             <figure>
-              <figure.icon className="text" />
+              {typeof (figure.icon) === 'function' &&
+              <figure.icon />}
+              {typeof (figure.icon) === 'string' &&
+              <img src={figure.icon} />}
               <figcaption>
                 <span>{figure.title}</span>
               </figcaption>
               {figure.button && (
-                <a className={`btn btn-${index % 2 == 0 ? "primary" : "secondary"} ${styles.button}`} href={figure.button.href} rel="noopener noreferrer" target="blank">
+                <a className={`btn btn-${index % 2 == 0 ? 'primary' : 'secondary'} ${styles.button}`} href={figure.button.href} rel='noopener noreferrer' target='blank'>
                   {figure.button.text}
                 </a>
               )}
               {figure.text && <span className={styles.text}>{figure.text}</span>}
             </figure>
           </Col>
-        );
-        if (index != figures.length - 1 && separator)
+        )
+
+        console.log(typeof figure.icon)
+        if (index != figures.length - 1 && separator) {
           return (
             <React.Fragment key={index}>
               {item}
-              <div style={{ borderLeft: "1px solid #570DAE" }}></div>
+              <div style={{ borderLeft: '1px solid #570DAE' }} />
             </React.Fragment>
-          );
-        else return item;
+          )
+        } else return item
       })}
     </div>
   </Container>
-);
+)
 
-export default injectIntl(Sash);
+export default injectIntl(Sash)
