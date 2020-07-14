39354b6d8a1334b9c4807548ecbcfe17fe8116ad
Ferit Tunçer <ferit@cryptolab.net>
2020-07-14 18:23:41 +0300

    fix regex matching juror redirection

--- src/pages/juror.js
+++ src/pages/juror.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-if (typeof window !== `undefined` && window.location.pathname.match(/\/\w{2}\/juror\/*/)) window.location.replace('https://blog.kleros.io/become-a-juror-blockchain-dispute-resolution-on-ethereum/'); // highlight-line
+if (typeof window !== `undefined` && window.location.pathname.match(/\/\w{0,2}\/?juror\/*/)) window.location.replace('https://blog.kleros.io/become-a-juror-blockchain-dispute-resolution-on-ethereum/'); // highlight-line
 
 const Juror = () => <></>;
 
