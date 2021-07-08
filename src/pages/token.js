333329d1da4dca7edf99f6061854e4a4a7d4ce8f
alcercu <alcercu@alumni.upv.es>
2021-07-08 12:19:41 +0200

    chore: maintenance exchanges token page

--- src/pages/token.js
+++ src/pages/token.js
@@ -4,16 +4,16 @@ import SEO from "../components/seo";
 import Sash from "../components/sash";
 import Deversifi from "src/assets/images/third-party-logos/deversifi.png";
 import Uniswap from "src/assets/svgs/uniswap.svg";
-import IDEX from "src/assets/images/third-party-logos/idex.png";
+import Sushiswap from "src/assets/images/third-party-logos/sushiswap.png";
 import OneInch from "src/assets/svgs/OneInch.svg";
 import Paraswap from "src/assets/images/third-party-logos/paraswap.jpg";
 import Balancer from "src/assets/svgs/balancer.svg";
 import Bitfinex from "src/assets/images/third-party-logos/bitfinex.png";
 import Kyber from "src/assets/images/third-party-logos/kyber.png";
-import Ninja from "src/assets/svgs/ninja-black-text.svg";
-import DexBlue from "src/assets/svgs/dexblue.svg";
 import Loopring from "src/assets/svgs/loopring.svg";
 import Transak from "src/assets/images/third-party-logos/transak.png";
+import GateIO from "src/assets/svgs/gateio.svg";
+import OKEX from "src/assets/svgs/okex.svg";
 import Kleros from "src/assets/svgs/icon-kleros.svg";
 import Attack from "src/assets/svgs/icon-thug.svg";
 import Governance from "src/assets/svgs/icon-gavel.svg";
@@ -87,42 +87,46 @@ const IndexPage = ({ intl }) => (
         </h1>
 
         <Container className="mt-5 d-flex flex-wrap justify-content-around">
-          <Badge as="a" className={styles.badge} href="https://www.bitfinex.com/t/PNKETH" rel="noopener noreferrer" target="blank">
-            <span class={styles.helper} />
-            <img src={Bitfinex} alt={"Bitfinex"} />
-          </Badge>
-          <Badge as="a" className={styles.badge} href="https://app.deversifi.com/" rel="noopener noreferrer" target="blank">
-            <span class={styles.helper} />
-            <img src={Deversifi} alt={"Deversifi"} />
-          </Badge>
-          <Badge as="a" className={styles.badge} href="https://uniswap.exchange/" rel="noopener noreferrer" target="blank">
+          <Badge as="a" className={styles.badge} href="https://app.uniswap.org/#/swap?inputCurrency=ETH&outputCurrency=0x93ed3fbe21207ec2e8f2d3c3de6e058cb73bc04d" rel="noopener noreferrer" target="blank">
             <Uniswap />
           </Badge>
-          <Badge as="a" className={styles.badge} href="https://loopring.org/" rel="noopener noreferrer" target="blank">
-            <Loopring />
+          <Badge as="a" className={styles.badge} href="https://app.sushi.com/swap?inputCurrency=ETH&outputCurrency=0x93ed3fbe21207ec2e8f2d3c3de6e058cb73bc04d" rel="noopener noreferrer" target="blank">
+            <img src={Sushiswap} alt={"Sushiswap"} />
           </Badge>
-          <Badge as="a" className={styles.badge} href="https://idex.market/eth/pnk" rel="noopener noreferrer" target="blank">
-            <span class={styles.helper} />
-            <img src={IDEX} alt={"IDEX"} />
+          <Badge as="a" className={styles.badge} href="https://balancer.exchange/" rel="noopener noreferrer" target="blank">
+            <Balancer />
           </Badge>
           <Badge as="a" className={styles.badge} href="https://kyberswap.com/swap" rel="noopener noreferrer" target="blank">
             <span class={styles.helper} />
             <img src={Kyber} alt={"Kyber"} />
           </Badge>
-
-          <Badge as="a" className={styles.badge} href="https://transak.com/" rel="noopener noreferrer" target="blank">
+          <Badge as="a" className={styles.badge} href="https://app.deversifi.com/" rel="noopener noreferrer" target="blank">
             <span class={styles.helper} />
-            <img src={Transak} alt={"Transak"} />
+            <img src={Deversifi} alt={"Deversifi"} />
           </Badge>
-          <Badge as="a" className={styles.badge} href="https://1inch.exchange/" rel="noopener noreferrer" target="blank">
-            <OneInch />
+          <Badge as="a" className={styles.badge} href="https://loopring.org/" rel="noopener noreferrer" target="blank">
+            <Loopring />
           </Badge>
           <Badge as="a" className={styles.badge} href="https://paraswap.io/" rel="noopener noreferrer" target="blank">
             <span class={styles.helper} />
             <img src={Paraswap} alt={"Paraswap"} />
           </Badge>
-          <Badge as="a" className={styles.badge} href="https://balancer.exchange/" rel="noopener noreferrer" target="blank">
-            <Balancer />
+          <Badge as="a" className={styles.badge} href="https://1inch.exchange/" rel="noopener noreferrer" target="blank">
+            <OneInch />
+          </Badge>
+          <Badge as="a" className={styles.badge} href="https://www.bitfinex.com/t/PNKETH" rel="noopener noreferrer" target="blank">
+            <span class={styles.helper} />
+            <img src={Bitfinex} alt={"Bitfinex"} />
+          </Badge>
+          <Badge as="a" className={styles.badge} href="https://www.gate.io/trade/PNK_USDT" rel="noopener noreferrer" target="blank">
+            <GateIO />
+          </Badge>
+          <Badge as="a" className={styles.badge} href="https://www.okex.com/markets/spot-info/pnk-usdt" rel="noopener noreferrer" target="blank">
+            <OKEX />
+          </Badge>
+          <Badge as="a" className={styles.badge} href="https://transak.com/" rel="noopener noreferrer" target="blank">
+            <span class={styles.helper} />
+            <img src={Transak} alt={"Transak"} />
           </Badge>
         </Container>
       </section>
