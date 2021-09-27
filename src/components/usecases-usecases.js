80e07baf805fab27528dc0a23f924e25a6182cfd
alcercu <alcercu@alumni.upv.es>
2021-09-27 15:55:56 +0200

    fix: change layout and don't show icon on low width viewports

--- src/components/usecases-usecases.js
+++ src/components/usecases-usecases.js
@@ -2,23 +2,27 @@ import React from 'react';
 
 import HorizontalCard from 'src/components/horizontal-card.js';
 import { injectIntl } from 'gatsby-plugin-intl';
-
+import useWindowDimensions from 'src/hooks/use-window-dimensions.js';
 import styles from 'src/components/styles/usecases-usecases.module.css'
 
-const UseCases = ({ titleID, figures, intl }) => (
-  <section className={`light ${styles.usecases}`}>
-    <h1>{intl.formatMessage({ id: titleID })}</h1>
-    <div className={styles.cards}>
-      {figures.map(({ icon, textID }) => (
-        <HorizontalCard
-          content={{
-            icon: icon,
-            text: intl.formatMessage({ id: textID })
-          }}
-        />
-      ))}
-    </div>
-  </ section>
-);
+const UseCases = ({ titleID, figures, intl }) => {
+  const { width } = useWindowDimensions();
+
+  return (
+    <section className={`light ${styles.usecases}`}>
+      <h1>{intl.formatMessage({ id: titleID })}</h1>
+      <div className={styles.cards}>
+        {figures.map(({ icon, textID }) => (
+          <HorizontalCard
+            content={{
+              icon: width > 336 ? icon : NaN,
+              text: intl.formatMessage({ id: textID })
+            }}
+          />
+        ))}
+      </div>
+    </ section>
+  )
+};
 
 export default injectIntl(UseCases);
